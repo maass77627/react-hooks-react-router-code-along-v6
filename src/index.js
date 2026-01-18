7b3af37072c924196f8b46dae5d54ee6904cc8a9
Kelly Maass <kmaass77627@gmail.com>
2026-01-18 13:41:23 -0600

    code along

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+function Home() {
+  return <h1>Home!</h1>;
+}
+
+function About() {
+  return <h1>This is my about component!</h1>;
+}
+
+function Login() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" />
+      </div>
+
+      <input type="submit" value="Login" />
+    </form>
+  );
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+);
